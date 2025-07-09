58615486fbc835f3f185e0032e2250b4a802c7f6
Przemek <przemekgg2002@gmail.com>
2025-07-10 00:05:46 +0200

    add: filmstrip test routine

--- s/demo/demo.bundle.ts
+++ s/demo/demo.bundle.ts
@@ -1,6 +1,6 @@
 
 import {Driver} from "../driver/driver.js"
-import {Filmstrip} from "../driver/utils/filmstrip.js"
+import {filmstripTest} from "./routines/filmstrip-test.js"
 import {setupTranscodeTest} from "./routines/transcode-test.js"
 
 const workerUrl = new URL("../driver/driver.worker.bundle.js", import.meta.url)
@@ -10,8 +10,6 @@ const results = document.querySelector(".results")!
 
 const fetchButton = document.querySelector(".fetch")
 const importButton = document.querySelector(".import") as HTMLButtonElement
-const slider = document.querySelector(".seconds") as HTMLInputElement
-const range = document.querySelector(".range")!
 
 fetchButton?.addEventListener("click", startDemoFetch)
 importButton?.addEventListener("click", startDemoImport)
@@ -28,33 +26,7 @@ async function startDemoImport()
 {
 	const [fileHandle] = await window.showOpenFilePicker()
 	const transcode = setupTranscodeTest(driver, fileHandle)
-	const container = document.querySelector("#filmstrip")!
-	const FPS_30 = 1000/30 / 1000
-	const filmstrip = await Filmstrip.init(
-			fileHandle,
-			{
-				onChange(tiles) {
-					// Sort by time (optional, for clean ordering)
-					const sorted = tiles.sort((a, b) => a.time - b.time)
-					// Clear previous thumbnails
-					container.replaceChildren(
-						...sorted.map(({ time, canvas }) => createLabeledCanvas(time, canvas.canvas as HTMLCanvasElement))
-					)
-				},
-				granularity: FPS_30,
-				canvasSinkOptions: {
-					width: 80,
-					height: 50,
-					fit: "fill"
-				}
-			}
-		)
-	slider.addEventListener("input", () => {
-		const [start, end] = [+slider.value, +slider.value+1]
-		filmstrip.update([start, end])
-		range.textContent = `visible time range: [${start}, ${end}]`
-	})
-	await filmstrip.update([10, 11])
+	await filmstripTest(fileHandle)
 	run(transcode, fileHandle.name)
 }
 
@@ -88,25 +60,4 @@ async function run(transcode: ReturnType<typeof setupTranscodeTest>, label: stri
 
 	// run the test
 	await transcode.run()
-}
-
-function createLabeledCanvas(time: number, canvas: HTMLCanvasElement) {
-	const wrapper = document.createElement('div')
-	wrapper.style.position = 'relative'
-	wrapper.style.display = 'inline-block'
-	wrapper.style.marginRight = '4px'
-	wrapper.appendChild(canvas)
-	const label = document.createElement('div')
-	label.textContent = `${time.toFixed(2)}s`
-	label.style.position = 'absolute'
-	label.style.top = '2px'
-	label.style.right = '4px'
-	label.style.fontSize = '10px'
-	label.style.color = 'white'
-	label.style.background = 'rgba(0,0,0,0.6)'
-	label.style.padding = '2px 4px'
-	label.style.borderRadius = '4px'
-	label.style.pointerEvents = 'none'
-	wrapper.appendChild(label)
-	return wrapper
 }
---
+++ s/demo/routines/filmstrip-test.ts
@@ -0,0 +1,68 @@
+import {Filmstrip} from "../../driver/utils/filmstrip.js"
+
+export async function filmstripTest(fileHandle: FileSystemFileHandle) {
+	const rangeSlider = document.querySelector(".range") as HTMLInputElement
+	const rangeView = document.querySelector(".range-view")!
+	const rangeSizeSlider = document.querySelector(".range-size")! as HTMLInputElement
+	const frequencySlider = document.querySelector(".frequency")! as HTMLInputElement
+	const frequencyView = document.querySelector(".frequency-view")!
+	const container = document.querySelector("#filmstrip")!
+	const FPS_10 = 1000/10 / 1000
+	let rangeSize = 0.5
+	const filmstrip = await Filmstrip.init(
+			fileHandle,
+			{
+				onChange(tiles) {
+					// Sort by time (optional, for clean ordering)
+					const sorted = tiles.sort((a, b) => a.time - b.time)
+					// Clear previous thumbnails
+					container.replaceChildren(
+						...sorted.map(({ time, canvas }) => createLabeledCanvas(time, canvas.canvas as HTMLCanvasElement))
+					)
+				},
+				granularity: FPS_10,
+				canvasSinkOptions: {
+					width: 80,
+					height: 50,
+					fit: "fill"
+				}
+			}
+		)
+	rangeSizeSlider.addEventListener("input", () => {
+		rangeSize = +rangeSizeSlider.value
+		const [start, end] = [+rangeSlider.value, +rangeSlider.value+rangeSize]
+		filmstrip.update([start, end])
+		rangeView.textContent = `visible time range: [${start}, ${end}]`
+	})
+	rangeSlider.addEventListener("input", () => {
+		const [start, end] = [+rangeSlider.value, +rangeSlider.value+rangeSize]
+		filmstrip.update([start, end])
+		rangeView.textContent = `visible time range: [${start}, ${end}]`
+	})
+	frequencySlider.addEventListener("input", () => {
+		filmstrip.granularity = 1000/+frequencySlider.value/1000
+		frequencyView.textContent = `frame every ${filmstrip.granularity.toFixed(3)} second (${frequencySlider.value} frames per second)`
+	})
+	await filmstrip.update([10, 10.5])
+}
+
+function createLabeledCanvas(time: number, canvas: HTMLCanvasElement) {
+	const wrapper = document.createElement('div')
+	wrapper.style.position = 'relative'
+	wrapper.style.display = 'inline-block'
+	wrapper.style.marginRight = '4px'
+	wrapper.appendChild(canvas)
+	const label = document.createElement('div')
+	label.textContent = `${time.toFixed(2)}s`
+	label.style.position = 'absolute'
+	label.style.top = '2px'
+	label.style.right = '4px'
+	label.style.fontSize = '10px'
+	label.style.color = 'white'
+	label.style.background = 'rgba(0,0,0,0.6)'
+	label.style.padding = '2px 4px'
+	label.style.borderRadius = '4px'
+	label.style.pointerEvents = 'none'
+	wrapper.appendChild(label)
+	return wrapper
+}

79507c87bfadac24c8aadfc1e5a7eb4fd52ca134
Chase Moskal <chasemoskal@gmail.com>
2025-08-07 01:53:22 -0700

    fix: transcriber worker

--- s/features/speech/transcribe/worker.bundle.ts
+++ s/features/speech/transcribe/worker.bundle.ts
@@ -7,33 +7,31 @@ import {loadPipe} from "./parts/load-pipe.js"
 import {transcribe} from "./parts/transcribe.js"
 import {TranscriberSchematic, TranscriberSpec} from "./types.js"
 
-await Comrade.worker<TranscriberSchematic>(shell => {
-	let deferred = defer<{pipe: Pipeline, spec: TranscriberSpec}>()
+const deferred = defer<{pipe: Pipeline, spec: TranscriberSpec}>()
 
-	const prepare = once(async(spec: TranscriberSpec) => {
-		deferred.resolve({
+const prepare = once(async(spec: TranscriberSpec) => {
+	deferred.resolve({
+		spec,
+		pipe: await loadPipe({
 			spec,
-			pipe: await loadPipe({
-				spec,
-				onLoading: loading => shell.host.loading(loading),
-			}),
-		})
+			onLoading: loading => host.loading(loading),
+		}),
 	})
-
-	return {
-		prepare,
-		async transcribe(request) {
-			const {pipe, spec} = await deferred.promise
-			return transcribe({
-				pipe,
-				spec,
-				request,
-				callbacks: {
-					onReport: report => shell.host.deliverReport(report),
-					onTranscription: transcription => shell.host.deliverTranscription(transcription),
-				},
-			})
-		},
-	}
 })
+
+const host = await Comrade.worker<TranscriberSchematic>(shell => ({
+	prepare,
+	async transcribe(request) {
+		const {pipe, spec} = await deferred.promise
+		return transcribe({
+			pipe,
+			spec,
+			request,
+			callbacks: {
+				onReport: report => shell.host.deliverReport(report),
+				onTranscription: transcription => shell.host.deliverTranscription(transcription),
+			},
+		})
+	},
+}))
 

78bf91d54c15c738155504928094292e195d0811
Przemek <przemekgg2002@gmail.com>
2025-10-11 02:35:48 +0200

    remove: obsolete code

--- s/tools/speech-recognition/whisper/parts/worker.bundle.ts
+++
@@ -1,7 +0,0 @@
-import {Comrade} from "@e280/comrade"
-
-import {setupWhisperWork} from "../fns/work.js"
-import {WhisperSchematic} from "../fns/schematic.js"
-
-await Comrade.worker<WhisperSchematic>(setupWhisperWork)
-

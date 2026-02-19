18d733e6ecb6e429a180a539ca61ca3923a1eaaa
Przemek Gałęzki <przemekgg2002@gmail.com>
2026-02-19 22:01:41 +0100

    add: more omni tests

--- s/timeline/sugar/omni.test.ts
+++ s/timeline/sugar/omni.test.ts
@@ -64,5 +64,125 @@ export default Science.suite({
 
 		expect(timelineDuration).is(videoA.duration)
 	}),
+	"nested stack inside sequence": test(async () => {
+		const {omni, videoA} = await setupTest()
+
+		const o = new O(omni.timeline(o => o.sequence(
+			o.video(videoA),
+			o.stack(
+				o.video(videoA),
+				o.video(videoA)
+			),
+			o.video(videoA),
+		)))
+
+		const timelineDuration = computeTimelineDuration(o.timeline.rootId, o.timeline)
+
+		expect(timelineDuration).is(videoA.duration * 3)
+	}),
+	"nested sequence inside stack": test(async () => {
+		const {omni, videoA} = await setupTest()
+
+		const o = new O(omni.timeline(o => o.stack(
+			o.video(videoA),
+			o.sequence(
+				o.video(videoA),
+				o.video(videoA)
+			)
+		)))
+
+		const timelineDuration = computeTimelineDuration(o.timeline.rootId, o.timeline)
+
+		expect(timelineDuration).is(videoA.duration * 2)
+	}),
+	"empty timeline duration is 0": test(async () => {
+		const {omni} = await setupTest()
+
+		const o1 = new O(omni.timeline(o => o.sequence()))
+		const o2 = new O(omni.timeline(o => o.stack()))
+		const duration1 = computeTimelineDuration(o1.timeline.rootId, o1.timeline)
+		const duration2 = computeTimelineDuration(o2.timeline.rootId, o2.timeline)
+
+		expect(duration1).is(0)
+		expect(duration2).is(0)
+	}),
+	"transition in a sequence": test(async () => {
+		const {omni, videoA} = await setupTest()
+
+		const transitionDuration = 1000
+
+		const o = new O(omni.timeline(o => o.sequence(
+			o.video(videoA),
+			o.transition.crossfade(transitionDuration),
+			o.video(videoA)
+		)))
+
+		const timelineDuration = computeTimelineDuration(o.timeline.rootId, o.timeline)
+
+		const expectedDuration = (videoA.duration * 2) - transitionDuration
+		expect(timelineDuration).is(expectedDuration)
+	}),
+	"transition in a stack": test(async () => {
+		const {omni, videoA} = await setupTest()
+
+		const transitionDuration = 1000
+
+		const o = new O(omni.timeline(o => o.stack(
+			o.video(videoA),
+			o.transition.crossfade(transitionDuration),
+			o.video(videoA)
+		)))
+
+		const timelineDuration = computeTimelineDuration(o.timeline.rootId, o.timeline)
+
+		expect(timelineDuration).is(videoA.duration)
+	}),
+	"ignore invalid transition": test(async () => {
+		const {omni, videoA} = await setupTest()
+
+		const o = new O(omni.timeline(o => o.sequence(
+			o.transition.crossfade(1000),
+			o.video(videoA),
+			o.video(videoA),
+			o.transition.crossfade(1000),
+		)))
+
+		const timelineDuration = computeTimelineDuration(o.timeline.rootId, o.timeline)
+
+		expect(timelineDuration).is(videoA.duration * 2)
+	}),
+	"clamp transition duration on overflow": test(async () => {
+		const {omni, videoA} = await setupTest()
+
+		const duration = 3000
+
+		const o = new O(omni.timeline(o => o.sequence(
+			o.video(videoA, {duration}),
+			o.transition.crossfade(duration * 2),
+			o.video(videoA, {duration})
+		)))
+
+		const timelineDuration = computeTimelineDuration(o.timeline.rootId, o.timeline)
+
+		expect(timelineDuration).is(duration)
+	}),
+	"multiple transitions": test(async () => {
+		const {omni, videoA} = await setupTest()
+		const transitionDuration = 1000
+
+		const o = new O(omni.timeline(o => o.sequence(
+			o.video(videoA),
+			o.transition.crossfade(transitionDuration),
+			o.video(videoA),
+			o.transition.crossfade(transitionDuration),
+			o.video(videoA)
+		)))
+
+		const timelineDuration = computeTimelineDuration(o.timeline.rootId, o.timeline)
+
+		const expected = (videoA.duration * 3) - (transitionDuration * 2)
+
+		expect(timelineDuration).is(expected)
+	}),
 })
 
